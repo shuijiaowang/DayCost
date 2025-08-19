dd99b41c4d8a18d077fad9a4132a8bf3067a1d9c
LY <2085580560@qq.com>
2025-08-19 17:03:30 +0800

    添加路由拦截

--- routes/router.go
+++ routes/router.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"DayCost/internal/handler"
+	"DayCost/pkg/middleware" // 导入中间件包
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,10 +18,17 @@ func SetupRouter() *gin.Engine {
 	{
 		userGroup.POST("/login", authHandler.Login)
 	}
-	expenseGroup := r.Group("/api/expenses")
+
+	// 需要认证的路由组
+	apiGroup := r.Group("/api")
+	apiGroup.Use(middleware.JWTInterceptor()) // 应用JWT拦截器
 	{
-		expenseGroup.POST("/", expenseHandler.AddExpense)
-		//expenseGroup.GET("/", expenseHandler.ListExpense)
+		// 消费记录路由（需要认证）
+		expenseGroup := apiGroup.Group("/expenses")
+		{
+			expenseGroup.POST("/", expenseHandler.AddExpense)
+			// expenseGroup.GET("/", expenseHandler.ListExpense)
+		}
 	}
 
 	return r
