a25762fb968e674b85c2f081a0e651566cb6b1c6
LY <2085580560@qq.com>
2025-08-29 16:37:29 +0800

    新增通用base_model

---
+++ internal/model/base_model.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type BaseModel struct {
+	CreatedAt time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;comment:创建时间"`
+	UpdatedAt time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;comment:更新时间"`
+	DeletedAt gorm.DeletedAt `gorm:"index;type:timestamp;comment:删除时间(软删除标志)"`
+}
--- internal/model/user.go
+++ internal/model/user.go
@@ -1,16 +1,8 @@
 package model
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type User struct {
-	ID        int            `gorm:"primaryKey;autoIncrement;comment:用户ID"`
-	Username  string         `gorm:"type:varchar(50);uniqueIndex;not null;comment:用户名"`
-	Password  string         `gorm:"type:varchar(100);not null;comment:加密密码"`
-	CreatedAt time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;comment:创建时间"`
-	UpdatedAt time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;comment:更新时间"`
-	DeletedAt gorm.DeletedAt `gorm:"index;type:timestamp;comment:删除时间(软删除标志)"`
+	ID       int    `gorm:"primaryKey;autoIncrement;comment:用户ID"`
+	Username string `gorm:"type:varchar(50);uniqueIndex;not null;comment:用户名"`
+	Password string `gorm:"type:varchar(100);not null;comment:加密密码"`
+	BaseModel
 }
