b4b2bb99dccb321320909bd6be8c8c77f261be9e
LY <2085580560@qq.com>
2025-08-30 18:00:18 +0800

    添加定时任务·

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"DayCost/pkg/corn"
 	"DayCost/pkg/database"
 	"DayCost/routes"
 	"log"
@@ -12,11 +13,18 @@ func main() {
 		log.Fatalf("数据库初始化失败: %v", err)
 	}
 
+	// 初始化定时任务
+	c, err := cron.Init()
+	if err != nil {
+		log.Fatalf("定时任务初始化失败: %v", err)
+	}
+	defer c.Stop() // 程序退出时停止
+
 	// 创建路由
 	r := routes.SetupRouter()
 
 	// 启动服务
-	if err := r.Run(":8080"); err != nil {
+	if err := r.Run(":8082"); err != nil {
 		log.Fatalf("服务启动失败: %v", err)
 	}
 }
